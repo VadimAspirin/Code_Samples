9e5bfa8337caba706b87bb25debfe4040f6eac2b
VadimAspirin <aspirine@protonmail.ch>
2018-07-26 20:56:44 +0500

    Update Program.cs

--- 3. chat/Client/Program.cs
+++ 3. chat/Client/Program.cs
@@ -9,25 +9,25 @@ namespace ChatClient
     {
         static void Main(string[] args)
         {
-			Console.Write("Введите свое имя: ");
-			if (args.Length == 2)
-			{
-				Chat chat = new Chat (Console.ReadLine(), args[0], Convert.ToInt32(args[1]));
-				Console.WriteLine("Добро пожаловать, {0}", chat.UserName);
-				for(int i = 0; i < 3; i++)
-					chat.SendMessage(Console.ReadLine());
-				Console.WriteLine("До свидания, {0}!", chat.UserName);
-				chat.Disconnect();
-			}
-			else
-			{
-				Chat chat = new Chat (Console.ReadLine());
-				Console.WriteLine("Добро пожаловать, {0}", chat.UserName);
-				for(int i = 0; i < 3; i++)
-					chat.SendMessage(Console.ReadLine());
-				Console.WriteLine("До свидания, {0}!", chat.UserName);
-				chat.Disconnect();
-			}
+		Console.Write("Введите свое имя: ");
+		if (args.Length == 2)
+		{
+			Chat chat = new Chat (Console.ReadLine(), args[0], Convert.ToInt32(args[1]));
+			Console.WriteLine("Добро пожаловать, {0}", chat.UserName);
+			for(int i = 0; i < 3; i++)
+				chat.SendMessage(Console.ReadLine());
+			Console.WriteLine("До свидания, {0}!", chat.UserName);
+			chat.Disconnect();
+		}
+		else
+		{
+			Chat chat = new Chat (Console.ReadLine());
+			Console.WriteLine("Добро пожаловать, {0}", chat.UserName);
+			for(int i = 0; i < 3; i++)
+				chat.SendMessage(Console.ReadLine());
+			Console.WriteLine("До свидания, {0}!", chat.UserName);
+			chat.Disconnect();
+		}
         }
     }
 }
